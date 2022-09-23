eaca476fb8062d578b53c6b96759cf5ebf533584
dfish19 <dfish19@yahoo.com>
2022-09-23 09:59:32 -0500

    initial

--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
+
     public class ChallengesSet05
     {
         public int GetNextNumberDivisibleByN(int startNumber, int n)
         {
-            throw new NotImplementedException();
+            startNumber++;
+
+            while (startNumber % n != 0)
+            {
+                startNumber++;
+            }
+            return startNumber;
         }
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
@@ -17,7 +25,12 @@ namespace ChallengesWithTestsMark8
 
         public bool IsAscendingOrder(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return false;
+            }
+            var num = numbers.OrderBy(x => x).ToArray();
+            return numbers.SequenceEqual(num);
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
@@ -27,17 +40,55 @@ namespace ChallengesWithTestsMark8
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            throw new NotImplementedException();
+            if (words == null || words.Length == 0)
+            {
+                return "";
+            }
+
+            string sentence = "";
+            foreach(var w in words)
+            {
+                if (w.Trim().Length > 0)
+                {
+                    sentence += w.Trim() + " ";
+                }
+            }
+
+            if (sentence.Length == 0)
+            {
+                return "";
+            }
+            return sentence.Trim() + ".";
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
         {
-            throw new NotImplementedException();
+            List<double> list = new List<double>();
+            if (elements == null || elements.Count == 0)
+            {
+                return list.ToArray();
+            }
+            for (int i = 3; i < elements.Count; i+= 4)
+            {
+                list.Add(elements[i]);
+            }
+            return list.ToArray();
         }
 
         public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int x = i + 1; x < nums.Length; x++)
+                {
+                    if (nums[i] + nums[x] == targetNumber)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
+
